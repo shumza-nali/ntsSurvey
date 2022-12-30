79fa09a8f1a66f8fc50a9d1c2de9ff9ca4a561cf
Humza Ali <hali@pragmaticsemi.com>
2022-12-30 23:33:20 +0000

    name change

--- R/nts_build.R
+++ R/nts_build.R
@@ -1,16 +1,11 @@
 #' Build the National Travel Survey data set from a list
 #'
-#' @param df_names
-#' @param df_cols
-#' @param year_min
-#' @param year_max
-#' @param nts_extract
+#' @param nts_dir Directory to folder where NTS .tab data files are stored
+#' @param nts_extract Default is a basic extract of NTS. Use a custom named list, e.g. basic_extract
 #'
-#' @return
+#' @return A tibble of NTS tables joined together
 #' @export
-#'
-#' @examples
-build_nts_list <- function(nts_dir, nts_extract = 'Default'){
+build_nts_list <- function(nts_dir, nts_extract = 'Default', nts_names = nts_names){
 
   if(!inherits(nts_extract , 'list')){
 
@@ -37,7 +32,14 @@ build_nts_list <- function(nts_dir, nts_extract = 'Default'){
 
 }
 
-build_nts_csv <- function(nts_dir, year_min = 2002, year_max = 2019, csv_dir = NULL){
+#' Build the National Travel Survey data set from a CSV input
+#'
+#' @param nts_dir Directory to folder where NTS .tab data files are stored
+#' @param csv_dir Directory to input csv file
+#'
+#' @return A tibble of NTS tables joined together
+#' @export
+build_nts_csv <- function(nts_dir, csv_dir = NULL, nts_names = nts_names){
 
   if(is.null(csv_dir)){
 
